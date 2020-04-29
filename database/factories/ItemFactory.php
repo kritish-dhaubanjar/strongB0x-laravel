c4832165b152ce639f693a38112e0747699ca826
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-04-29 12:42:37 +0545

    Purchase & Sales Price Adjusted

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -8,13 +8,14 @@ use Illuminate\Support\Str;
 
 $factory->define(Item::class, function (Faker $faker) {
 	$faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+	$purchase_price = $faker->numberBetween($min = 1, $max = 100);
 
     return [
         // 'name' => Str::random(16),
         'name' => $faker->productName,
         'description' => $faker->paragraph,
-        'sale_price' => $faker->numberBetween($min = 1, $max = 100),
-        'purchase_price' => $faker->numberBetween($min = 1, $max = 100),
+        'sale_price' => $purchase_price + $faker->numberBetween($min = 1, $max = 100),
+        'purchase_price' => $purchase_price,
         'unit_id'=> $faker->numberBetween($min=1, $max = 5),
         'category_id'=> 4,
         'tax_id'=>2
