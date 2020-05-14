4915939de360bd62bc7bd482e601d00c3e118a20
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-05-14 19:35:57 +0545

    casted ids to integer

--- app/User.php
+++ app/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role_id' => 'integer'
     ];
 
     public function role()
